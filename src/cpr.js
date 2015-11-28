807941f0829313a25aca8752500369689999cb58
Brandon Wilhite <brandonjwilhite@gmail.com>
2015-11-28 11:08:02 -0500

    fix problem when trying to copy single file

--- src/cpr.js
+++ src/cpr.js
@@ -9,17 +9,27 @@ see: https://github.com/jprichardson/node-fs-extra#copy
 */
 //example usage; will copy all files EXCEPT .js or .md files into dist
 //node cpr.js ../src ../dist '**/!(*.js|*.md)'
-//
 
 const fs = require('fs-extra')
 const minimatch = require("minimatch")
 const chalk = require('chalk');
 const args = process.argv
-console.log('cpr.js:')
-let matcher = ()=>{}
+const showSeparator = ()=>{
+    console.log('----------------------')
+}
+
+showSeparator()
+console.log(chalk.white.bgBlue.bold('Begin cpr.js:'))
+
+let matcher = function matchAll (filename){
+    const match = minimatch(filename, '**/**')
+    const msg = 'copy ' + filename + ' -> ' + match
+    console.log(match ? msg : chalk.yellow(msg))
+    return match
+}
 if(args[4]){
-    console.log('glob pattern: ' + args[4])
-    matcher = (filename)=>{
+    console.log(chalk.bold('glob pattern: ' + args[4]))
+    matcher = function matchGlob (filename){
         const match = minimatch(filename, args[4])
         const msg = 'copy ' + filename + ' -> ' + match
         console.log(match ? msg : chalk.yellow(msg))
@@ -27,7 +37,15 @@ if(args[4]){
     }
 }
 
-fs.copy(args[2], args[3], {filter: matcher}, function (err) {
-  if (err) return console.error(err)
-  console.log('cpr.js: success')
+fs.copy(args[2], args[3], {filter: matcher, clobber: true, preserveTimestamps: false}, function (err) {
+    if (err){
+        console.error(err)
+        console.log(chalk.red.bold('cpr.js: error'))
+    }else{
+        console.log(chalk.bold('cpr.js: success'))
+    }
+
+    console.log(chalk.white.bgBlue.bold('End cpr.js:'))
+    showSeparator()
+    return (err ? err : 0)
 }) // copies directory, even if it has subdirectories or files
--- src/scaffold.js
+++ src/scaffold.js
@@ -1,6 +1,6 @@
-var fs = require('fs-extra')
+const fs = require('fs-extra')
 
-var dir = '/src/lib'
+const dir = '/src/lib'
 fs.ensureDir(dir, function (err) {
   console.log(err) // => null
   // dir has now been created, including the directory it is to be placed in
